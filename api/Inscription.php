ee83b167181c126901f30e07d5e4d6f32a5dcf08
Oussama-Laaziz07 <ousslz1607@gmail.com>
2026-04-04 15:08:29 +0100

    fix9

--- api/Inscription.php
+++ api/Inscription.php
@@ -102,7 +102,7 @@ input[type="radio"] {
 </head>
 <body>
     <h1>Inscription des étudiants</h1>
-    <form method="Post" action="api/Inscription">
+    <form method="POST" action="Inscription.php">
         <table>
             <tr>
                 <td>Numéro d'inscription : <input type="number" name="ins" id="insID"></td>
