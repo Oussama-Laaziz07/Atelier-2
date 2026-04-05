dd57aed4d33c0fa1d1d3d485c5ae44c8c6464ac7
Oussama-Laaziz07 <ousslz1607@gmail.com>
2026-04-05 19:24:05 +0100

    fix13

--- api/Inscription.php
+++ api/Inscription.php
@@ -101,7 +101,7 @@ input[type="radio"] {
 </head>
 <body>
     <h1>Inscription des étudiants</h1>
-    <form method="POST" action="https://atelier-2-omega.vercel.app/api/information.php">
+    <form method="post" action="information.php">
         <table>
             <tr>
                 <td>Numéro d'inscription : <input type="number" name="ins" id="insID"></td>
